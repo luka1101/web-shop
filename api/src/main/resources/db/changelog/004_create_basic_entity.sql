94d45d4c9b152865c923acdd7d89ac99b0192b15
luka11 <luka.dunjic@qcerris.com>
2024-03-28 14:59:03 +0100

    Dropped test table in order to start creating database.

---
+++ api/src/main/resources/db/changelog/004_create_basic_entity.sql
@@ -0,0 +1,3 @@
+create table basic_entity(
+    id integer auto_increment primary key
+);
---
+++ api/src/main/resources/db/changelog/005_create_pay_pal_account_entity.sql
@@ -0,0 +1,5 @@
+create table pay_pal_account(
+    accountId integer auto_increment primary key,
+    expiresOn date,
+    budget double
+)
