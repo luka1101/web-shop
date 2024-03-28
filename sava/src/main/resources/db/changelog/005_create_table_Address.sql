6918fbc968a3aad74647afcf95486c521ef58424
SavaAndricQcerris <sava.andric@qcerris.com>
2024-03-28 15:47:52 +0100

    Added DB Tables Sava

---
+++ sava/src/main/resources/db/changelog/005_create_table_Address.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Address (
+     country VARCHAR(255),
+     city VARCHAR(255),
+     postalCode INT,
+     street VARCHAR(255)
+);
---
+++ sava/src/main/resources/db/changelog/006_create_table_PayPalAccount.sql
@@ -0,0 +1,11 @@
+CREATE TABLE PayPalAccount (
+       accountId VARCHAR(255) PRIMARY KEY,
+       expiresOn DATE,
+       budget DOUBLE,
+       country VARCHAR(255),
+       city VARCHAR(255),
+       postalCode INT,
+       street VARCHAR(255),
+       user VARCHAR(255),
+       FOREIGN KEY (user) REFERENCES User(username)
+);
